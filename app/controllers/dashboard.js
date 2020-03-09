e7f8ce71bd7687f634ced0065c3ed289e5e55654
Logan Schmidt <d4rkd0s@gmail.com>
2020-03-09 17:56:31 -0500

    WIP fixed JS

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -7,36 +7,31 @@ $(document).ready(() =>{
 
     console.log(name)
 
+    $("#main-list-group > a").on("click", () => {
+        // Remove the current active link
+        $("#main-list-group > .active").removeClass("active");
+    }
+
     // functions to define button click actions for tools
     $("#tool1").on("click", () =>{
-        // Clear the current active link
-        $("#main-list-group > .active").removeClass("active");
         // Set tool 1 as the active link
-        $("#main-list-group > #tool1").addClass("active");
+        $("#tool1").addClass("active");
     });
     $("#tool2").on("click", () =>{
-        // Clear the current active link
-        $("#main-list-group > .active").removeClass("active");
         // Set tool 1 as the active link
-        $("#main-list-group > #tool1").addClass("active");
+        $("#tool2").addClass("active");
     });
     $("#tool3").on("click", () =>{
-        // Clear the current active link
-        $("#main-list-group > .active").removeClass("active");
         // Set tool 1 as the active link
-        $("#main-list-group > #tool1").addClass("active");
+        $("#tool3").addClass("active");
     });
     $("#tool4").on("click", () =>{
-        // Clear the current active link
-        $("#main-list-group > .active").removeClass("active");
         // Set tool 1 as the active link
-        $("#main-list-group > #tool1").addClass("active");
+        $("#tool4").addClass("active");
     });
     $("#tool5").on("click", () =>{
-        // Clear the current active link
-        $("#main-list-group > .active").removeClass("active");
         // Set tool 1 as the active link
-        $("#main-list-group > #tool1").addClass("active");
+        $("#tool5").addClass("active");
     });
 
 
